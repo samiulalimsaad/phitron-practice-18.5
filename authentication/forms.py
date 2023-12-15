1f226c620d0259f06b3a33a0fc5fc23602ca0984
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-16 00:12:45 +0600

    ChangePasswordForm added

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,5 @@
-# myapp/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import SetPasswordForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -10,3 +9,21 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class ChangePasswordForm(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Exclude old_password field from rendering
+        self.fields.pop("old_password", None)
+
+    new_password1 = forms.CharField(
+        label="New Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+    )
+    new_password2 = forms.CharField(
+        label="Confirm New Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+    )
